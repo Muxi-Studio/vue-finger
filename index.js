06a9fe2f52ab6c501ef5b57863fd2ae184b19757
Elegenthus <996886231@qq.com>
2016-11-27 15:58:11 +0800

    add pintch

--- index.js
+++ index.js
@@ -10,35 +10,78 @@ var vueFingerConstructor = function(el, options) {
     this.x1 = this.x2 = this.y1 = this.y2 = null;
     this.swipe = options.swipe
     this.swipeMove = options.swipeMove
+    this.pintch = options.pintch
+    this.pintchDistance = 50
 }
 
 vueFingerConstructor.prototype = {
     start: function(e) {
+        e.preventDefault()
         if (!e.touches) return
         this.x1 = e.touches[0].pageX
         this.y1 = e.touches[0].pageY
+        if(e.touches.length > 1){
+            console.log("scale:",e.customscale)
+            this.p1 = e.touches[1].pageX
+            this.q1 = e.touches[1].pageY
+            this.pintchDistance1 = Math.sqrt(Math.pow((this.x1-this.p1),2) + Math.pow((this.y1-this.q1),2))
+        }
+        
+
         this.previous = {
             x1: this.x1,
             y1: this.y1
         }
         e.distanceX = 0 
         e.distanceY = 0
+        
     },
     move: function(e) {
+        e.preventDefault()
+ 
         var currentX = e.touches[0].pageX,
-            currentY = e.touches[0].pageY
+            currentY = e.touches[0].pageY;
+            if(e.touches.length > 1){
+                currentP = e.touches[1].pageX,
+                currentQ = e.touches[1].pageY,
+                this.p2 = currentP,
+                this.q2 = currentQ
+            }
+
         this.x2 = currentX
         this.y2 = currentY
+        this.p2 = currentP
+        this.q2 = currentQ
 		
         e.distanceX = this.x2 - this.x1  
         e.distanceY = this.y2 - this.y1
+        if(e.touches.length > 1){
+            this.pintchDistance2 = Math.sqrt(Math.pow((this.x2 - this.p2),2) + Math.pow((this.y2 - this.q2),2))
+            if(this.pintchDistance2 > this.pintchDistance1){
+                e.customscale = 1 + (this.pintchDistance2 - this.pintchDistance1)/this.pintchDistance
+            }else{
+                if(e.customscale - (this.pintchDistance1 - this.pintchDistance2)/this.pintchDistance < 1){
+                    e.customscale = 10.0
+                    console.log(e)
+                }else{
+                console.log("jaajaj")
+                    e.customscale -= (this.pintchDistance1 - this.pintchDistance2)/this.pintchDistance
+                }
+            }
+            if (this.pintch) {
+                this.pintch(e)
+            }
+        }
+        
 
         e.deltaX = currentX - this.previous.x1
         e.deltaY = currentY - this.previous.y1
         
         this.previous = {
             x1: currentX,
-            y1: currentY
+            y1: currentY,
+            p1: currentP,
+            q1: currentQ
         }
 
         if ((currentX && Math.abs(e.distanceX) > 30) ||
@@ -75,6 +118,7 @@ vueFingerConstructor.prototype = {
 var vueFinger = {}
 vueFinger.swipeArr = []
 vueFinger.swipeMoveArr = []
+vueFinger.pintchArr = []
 
 vueFinger.install = function(Vue, options) {
 
@@ -93,6 +137,12 @@ vueFinger.install = function(Vue, options) {
                 })
                 vueFinger.swipeMoveArr.push(ins)
             }
+            if (binding.arg === "pintch") {
+                var ins = new vueFingerConstructor(el, {
+                    pintch: binding.value
+                })
+                vueFinger.pintchArr.push(ins)
+            }
         },
         unbind: function() {
             vueFinger.swipeArr.forEach(function(item) {
@@ -100,6 +150,10 @@ vueFinger.install = function(Vue, options) {
                 item = null
             })
             vueFinger.swipeMoveArr.forEach(function(item) {
+                item.detach()
+                item = null
+            })
+            vueFinger.pintchArr.forEach(function(item) {
                 item.detach()
                 item = null
             })
